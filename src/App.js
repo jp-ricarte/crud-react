5294cecd87339bf33cf0eacee682abe66397917e
Joao Pedro <joaopedroricarte@hotmail.com>
2019-11-06 13:51:42 -0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 
-export default class src extends Component {
+
+export default class App extends Component {
+
+    state = {
+      value: ''
+    };
+  
+    handleChange =(e)=>{
+      this.setState({value: e.target.value});
+    }
+
+    handleSubmit = (e) =>{
+      e.preventDefault();
+      console.log(this.state.value);
+ 
+    }
   render() {
-    return <div />;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+           />
+          <button onClick={this.handleSubmit}>OK</button>
+        </form>
+      </div>
+    );
   }
 }
 ;
