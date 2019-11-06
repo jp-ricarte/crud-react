b70c93f1b67813e78f842a810b6631da51fcb42a
Joao Pedro <joaopedroricarte@hotmail.com>
2019-11-06 19:39:52 -0300

    add div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
-
-
 export default class App extends Component {
-
-    state = {
-      value: ''
-    };
   
+        state = {
+      value: '',
+      array: [],
+  }
+    
     handleChange =(e)=>{
       this.setState({value: e.target.value});
     }
@@ -15,22 +14,39 @@ export default class App extends Component {
     handleSubmit = (e) =>{
       e.preventDefault();
       console.log(this.state.value);
- 
-    }
+      this.setState(state => {
+        const array = state.array.concat(this.state.value);
+       
+        return {
+          array,
+        }
+      });
+      this.setState({value: ''});
+    };
+    
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+      
+        <form>
+        
           <input 
           type="text"
           value={this.state.value}
           onChange={this.handleChange}
            />
-          <button onClick={this.handleSubmit}>OK</button>
-        </form>
+          <button onClick={this.handleSubmit} >OK</button>
+          </form>
+          <div className="div2">  
+        <ul>
+          {this.state.array.map(nome => (
+            <li key={nome}>{nome}</li>
+            ))}
+          </ul>
+          </div>  
       </div>
     );
   }
-}
-;
+};
+
 
