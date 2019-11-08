e7db4d327676822b022606f4db71452b0810048f
Joao Pedro <joaopedroricarte@hotmail.com>
2019-11-08 00:19:22 -0300

    add validation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ export default class App extends Component {
         state = {
       value: '',
       array: [],
-  }
+      error: "",
     
+  }
     handleChange =(e)=>{
       this.setState({value: e.target.value});
+    
     }
 
-    handleSubmit = (e) =>{
+    handleSubmit = (e) =>{ 
+
+      this.setState({error:''})
+
+      if (this.state.value === ""){
+        this.setState({error: "Preencha o campo!"})
+
+        e.preventDefault();
+         return;  
+    }
+     
       e.preventDefault();
-      console.log(this.state.value);
+      
+
       this.setState(state => {
         const array = state.array.concat(this.state.value);
        
@@ -22,25 +35,26 @@ export default class App extends Component {
         }
       });
       this.setState({value: ''});
+      
     };
     
   render() {
     return (
-      <div>
-      
+      <div className="mainDiv">
         <form>
-        
           <input 
           type="text"
           value={this.state.value}
           onChange={this.handleChange}
            />
           <button onClick={this.handleSubmit} >OK</button>
+          <div className="error">{this.state.error}</div>
           </form>
           <div className="div2">  
         <ul>
+        
           {this.state.array.map(nome => (
-            <li key={nome}>{nome}</li>
+            <li key={nome} ><input className="checkbox" type="checkbox" />{nome}</li>
             ))}
           </ul>
           </div>  
